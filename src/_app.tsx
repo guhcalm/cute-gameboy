9987b28b14b516e0bfbfd23f17cb174786b03f17
guhcalm <guhcalm@outlook.com>
2022-06-04 17:03:53 -0400

    main

--- src/_app.tsx
+++ src/_app.tsx
@@ -3,3 +3,92 @@ const GameboyApp = () => {
 }
 
 export default GameboyApp
+/*
+enum StatusEnum {
+  Active = "ACTIVE",
+  Inactive = "INACTIVE"
+}
+interface ColorInterface {
+  light?: string
+  main?: string
+  dark?: string
+  on?: string
+}
+interface ThemeInterface {
+  name: string
+  palette: {
+    primary: ColorInterface
+    secondary: ColorInterface
+    surface: ColorInterface
+    background: ColorInterface
+    error: ColorInterface
+  }
+}
+interface DirectionalPadInterface {
+  left: StatusEnum
+  right: StatusEnum
+  up: StatusEnum
+  down: StatusEnum
+}
+interface OperationalPadInterface {
+  select: StatusEnum
+  start: StatusEnum
+}
+interface ActionPadInterface {
+  select: StatusEnum
+  start: StatusEnum
+}
+interface HardwareInterface {
+  status: StatusEnum
+  theme: ThemeInterface
+  keyPads: {
+    startupSwitcher: StatusEnum
+    directionalPad: DirectionalPadInterface
+    operationalPad: OperationalPadInterface
+    actionPad: ActionPadInterface
+  }
+}
+interface KeyboardInterface {
+  startupSwitcher: StatusEnum
+  directionalPad: DirectionalPadInterface
+  operationalPad: OperationalPadInterface
+  actionPad: ActionPadInterface
+}
+interface SystemInterface {
+  status: StatusEnum
+  keyboard: {
+    startupSwitcher: StatusEnum
+    directionalPad: DirectionalPadInterface
+    operationalPad: OperationalPadInterface
+    actionPad: ActionPadInterface
+  }
+  settings: {
+    theme: ThemeInterface
+  }
+  user: {
+    status: StatusEnum
+    profile: {
+      name: string
+      score: number
+    }
+  }
+}
+
+interface GameboyInterface {
+  status: StatusEnum
+  keys: {
+    startupSwitcher: StatusEnum
+    directionalPad: DirectionalPadInterface
+    operationalPad: OperationalPadInterface
+    actionPad: ActionPadInterface
+  }
+  settings: {
+    theme: ThemeInterface
+  }
+  domains: {
+    keyboard: KeyboardInterface
+    hardware: HardwareInterface
+    system: SystemInterface
+  }
+}
+*/
--- webpack.config.ts
+++ webpack.config.ts
@@ -1,10 +1,14 @@
 import HTML from "html-webpack-plugin"
 import Mini from "terser-webpack-plugin"
 
-export default ({ WEBPACK_SERVE }) => ({
-  mode: WEBPACK_SERVE && "development",
-  plugins: [new HTML({ template: "public/index.html" })],
-  resolve: { extensions: [".js", ".jsx", ".ts", ".tsx"] },
+const developmentConfig = {
+  mode: "development",
+  devtool: "inline-source-map",
+  devServer: { port: 3000, open: false, hot: true }
+}
+
+const productionConfig = {
+  mode: "production",
   output: {
     assetModuleFilename: "assets/[name].[fullhash].[ext]",
     filename: "assets/[name].[fullhash].js",
@@ -22,12 +26,22 @@ export default ({ WEBPACK_SERVE }) => ({
     },
     minimize: true,
     minimizer: [new Mini()]
-  },
+  }
+}
+
+const defaultConfig = {
   module: {
     rules: [
       { test: /\.(js|ts)x?$/, use: "babel-loader" },
       { test: /\.(png|jpg|jpeg|webp|gif)$/, type: "asset/resource" },
       { test: /\.(woff|eot|ttf|oft|svg|ico)$/, type: "asset/inline" }
     ]
-  }
+  },
+  plugins: [new HTML({ template: "public/index.html" })],
+  resolve: { extensions: [".js", ".jsx", ".ts", ".tsx"] }
+}
+
+export default ({ WEBPACK_SERVE }) => ({
+  ...defaultConfig,
+  ...(WEBPACK_SERVE ? developmentConfig : productionConfig)
 })
