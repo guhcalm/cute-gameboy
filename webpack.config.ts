d95fc338bac52f09d0037c5792b86f813f0772e5
guhcalm <guhcalm@outlook.com>
2022-06-04 18:02:36 -0400

    main

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,37 +1,40 @@
 import HTMLPlugin from "html-webpack-plugin"
 import MinifyPlugin from "terser-webpack-plugin"
 
-const defaultConfig = {
-  plugins: [new HTMLPlugin({ template: "public/index.html" })],
-  resolve: { extensions: [".js", ".jsx", ".ts", ".tsx"] },
-  module: { rules: [
-    { test: /\.(js|ts)x?$/, use: "babel-loader" },
-    { test: /\.(png|jpg|jpeg|webp|gif)$/, type: "asset/resource" },
-    { test: /\.(woff|eot|ttf|oft|svg|ico)$/, type: "asset/inline" }    
-  ]}
-}
-
-const developmentConfig = {
-  mode: "development",
-  devtool: "inline-source-map",
-  devServer: { port: 3000, open: false, hot: true }
-}
-
-const productionConfig = {
-  mode: "production",
-  output: {
-    assetModuleFilename: "assets/[name].[fullhash].[ext]",
-    filename: "assets/[name].[fullhash].js", clean: true
+const myConfig = {
+  profuction: {
+    mode: "production",
+    enty: { main: "./src/index.tsx" },
+    output: {
+      assetModuleFilename: "assets/[name].[fullhash].[ext]",
+      filename: "assets/[name].[fullhash].js", clean: true
+    },
+    optimization: {
+      splitChunks: { cacheGroups: { libs: {
+        chunks: "all", name: "libs", test: /[\\/]node_modules[\\/]/
+      }}},
+      minimize: true, minimizer: [new MinifyPlugin()]
+    } 
   },
-  optimization: {
-    splitChunks: { cacheGroups: { libs: {
-      chunks: "all", name: "libs", test: /[\\/]node_modules[\\/]/
-    }}},
-    minimize: true, minimizer: [new MinifyPlugin()]
+  development: {
+    mode: "development",
+    devtool: "inline-source-map",
+    devServer: { port: 3000, open: false, hot: true }
+  },
+  default: {
+    plugins: [new HTMLPlugin({ template: "public/index.html" })],
+    resolve: { extensions: [".js", ".jsx", ".ts", ".tsx"] },
+    module: { rules: [
+      { test: /\.(js|ts)x?$/, use: "babel-loader" },
+      { test: /\.(png|jpg|jpeg|webp|gif)$/, type: "asset/resource" },
+      { test: /\.(woff|eot|ttf|oft|svg|ico)$/, type: "asset/inline" }    
+    ]}
   }
 }
 
-export default ({ WEBPACK_SERVE }) => ({
-  ...defaultConfig,
-  ...(WEBPACK_SERVE ? developmentConfig : productionConfig)
+export default ({ WEBPACK_BUILD }) => ({
+  ...myConfig.default,
+  ...(WEBPACK_BUILD
+    ? myConfig.profuction
+    : myConfig.development)
 })
