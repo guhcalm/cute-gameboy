32cafd59468db37ee71d0d480a4da24e0d641900
guhcalm <guhcalm@outlook.com>
2022-06-05 07:50:20 -0400

    main

--- src/styles/themes/interfaces.ts
+++ src/styles/themes/interfaces.ts
@@ -19,3 +19,118 @@ export interface ThemeInterface {
     secondary: ColorInterface
   }
 }
+
+export enum StatusEnum {
+  Active = "ACTIVE",
+  Inactive = "INACTIVE"
+}
+
+/**
+ * @DirectionalPad
+ */
+enum DirectionalPadEnum {
+  Left = "LEFT",
+  Right = "RIGHT",
+  Up = "UP",
+  Down = "DOWN"
+}
+interface DirectionalPadInterface {
+  left: DirectionalPadEnum.Left
+  right: DirectionalPadEnum.Right
+  up: DirectionalPadEnum.Up
+  down: DirectionalPadEnum.Down
+}
+
+/**
+ * @OperationalPad
+ */
+enum OperationalPadEnum {
+  Select = "SELECT",
+  Start = "START"
+}
+interface OperationalPadInterface {
+  select: OperationalPadEnum.Select
+  start: OperationalPadEnum.Start
+}
+
+/**
+ * @ActionPad
+ */
+enum ActionPadEnum {
+  E = "E",
+  Q = "Q"
+}
+interface ActionPadInterface {
+  e: ActionPadEnum.E
+  q: ActionPadEnum.Q
+}
+
+/**
+ * @Settings
+ */
+interface SettingsInterface {
+  theme: ThemeInterface
+}
+
+/**
+ * @User
+ */
+interface UserInterface {
+  status: StatusEnum
+  profile: { name: string; score: number }
+}
+
+/**
+ * @Mouse
+ */
+interface MouseInterface {
+  position: {
+    x: number
+    y: number
+  }
+}
+
+/**
+ * @Keypad
+ */
+interface KeypadInterface {
+  startupSwitcher: StatusEnum
+  directionalPad: DirectionalPadInterface
+  operationalPad: OperationalPadInterface
+  actionPad: ActionPadInterface
+}
+
+/**
+ * @Hardware
+ */
+interface HardwareInterface {
+  status: StatusEnum
+  settings: SettingsInterface
+  keypad: KeypadInterface
+}
+
+/**
+ * @System
+ */
+interface SystemInterface {
+  status: StatusEnum
+  keypad: KeypadInterface
+  user: UserInterface
+  settings: SettingsInterface
+}
+
+/**
+ * @Gameboy_Core
+ */
+interface GameboyInterface {
+  status: StatusEnum
+  settings: SettingsInterface
+  mouse: MouseInterface
+  keys: KeypadInterface
+  domains: {
+    mouse: MouseInterface
+    keyboard: KeypadInterface
+    hardware: HardwareInterface
+    system: SystemInterface
+  }
+}
