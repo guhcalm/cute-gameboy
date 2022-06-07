ae9a4e84e022c48229683dc8d721c85265930f6d
guhcalm <guhcalm@outlook.com>
2022-06-07 10:55:01 -0400

    main

--- src/.domains/hardware/components/OperationalPad/index.tsx
+++ src/.domains/hardware/components/OperationalPad/index.tsx
@@ -1,14 +1,21 @@
+import { useDispatch } from "react-redux"
 import Container, { Button } from "./style"
-
-export default () => (
-  <Container>
-    <Button>
-      <button></button>
-      <span>SELECT</span>
-    </Button>
-    <Button>
-      <button onClick={}></button>
-      <span>START</span>
-    </Button>
-  </Container>
-)
+import {hardwareSlice} from "../../store/reducers"
+export default () => {
+  const dispatch = useDispatch()
+  const {actions} = hardwareSlice
+  const clickDown = () => dispatch(actions.clickStartupSwitcher())
+  const clickUp = () => dispatch(actions.unclickStartupSwitcher())
+  return (
+    <Container>
+      <Button>
+        <button></button>
+        <span>SELECT</span>
+      </Button>
+      <Button>
+        <button onMouseDown={clickDown} onMouseUp={clickUp}></button>
+        <span>START</span>
+      </Button>
+    </Container>
+  )
+}
