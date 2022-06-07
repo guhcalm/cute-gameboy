26456ca0b024f9bd94f0d5cf0d212303dd757115
guhcalm <guhcalm@outlook.com>
2022-06-06 20:02:09 -0400

    main

--- src/_app.tsx
+++ src/_app.tsx
@@ -1,20 +1,16 @@
 import { useSelector } from "react-redux"
 import { Gameboy } from "./containers"
 import { MouseApp, KeyboardApp } from "./.domains"
-import {
-  useKeypadManager,
-  useMousePositionManager,
-  useStatusManager
-} from "./hooks"
+import * as hooks from "./hooks"
 import { GlobalInterfaces } from "./interfaces"
 
 const GameboyApp = () => {
   MouseApp()
   KeyboardApp()
 
-  // useStatusManager()
-  useMousePositionManager()
-  // useKeypadManager()
+  hooks.useMousePosition()
+  // useStatus()
+  // useKeypad()
 
   const state = useSelector(
     (store: GlobalInterfaces.GlobalInterface) => store.gameboy.mouse.position
